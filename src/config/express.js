07c4f4bd99d37c5b9e64c2ae3eed4f6abd735c05
everton2803 <everton.serighelli@gmail.com>
2025-10-09 14:16:49 -0300

    first commit

---
+++ src/config/express.js
@@ -0,0 +1,11 @@
+// src/config/express.js
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+module.exports = app;
+
+// Middleware básicos do Express
+app.use(express.json()); // Middleware para interpretar JSON
+app.use(express.urlencoded({ extended: true })); // Suporte para dados de formulários
+app.use(morgan("combined")); // Logging HTTP
