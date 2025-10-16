ac585fc81c45480060e34837ecfc801812079af4
Everton Jose Serighelli <everton.serighelli@gmail.com>
2025-10-16 11:27:22 -0300

    v4

--- src/routes/livros.routes.js
+++ src/routes/livros.routes.js
@@ -6,9 +6,16 @@ const livrosController = new LivrosController();
 // Middlewares
 const { validarLivro, validarParamId } = require("../middleware/validar/livros.validar");
 
-router.get("/", (req, res, next) => livrosController.listarLivros.bind(req, res, next));
-router.get("/:id", validarParamId, (req, res, next) => livrosController.buscarLivroPorId.bind(req, res, next));
-router.post("/", validarLivro, (req, res, next) => livrosController.criarLivro.bind(req, res, next));
-router.put("/:id", validarParamId, validarLivro, (req, res, next) => livrosController.atualizarLivro.bind(req, res, next));
-router.delete("/:id", validarParamId, (req, res, next) => livrosController.removerLivro.bind(req, res, next));
+//router.get("/", (req, res, next) => livrosController.listarLivros(req, res, next));
+//router.get("/:id", validarParamId, (req, res, next) => livrosController.buscarLivroPorId(req, res, next));
+//router.post("/", validarLivro, (req, res, next) => livrosController.criarLivro(req, res, next));
+//router.put("/:id", validarParamId, validarLivro, (req, res, next) => livrosController.atualizarLivro(req, res, next));
+//router.delete("/:id", validarParamId, (req, res, next) => livrosController.removerLivro(req, res, next));
+
+router.get("/", livrosController.listarLivros.bind(livrosController));
+router.get("/:id", validarParamId, livrosController.buscarLivroPorId.bind(livrosController));
+router.post("/", livrosController.criarLivro.bind(livrosController));
+router.put("/:id", validarParamId, validarLivro, livrosController.atualizarLivro.bind(livrosController));
+router.delete("/:id", validarParamId, livrosController.removerLivro.bind(livrosController));
+
 module.exports = router;
