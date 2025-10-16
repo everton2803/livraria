4320aa433709f7d2befc6fa476feacf5ab7ed366
Everton Jose Serighelli <everton.serighelli@gmail.com>
2025-10-16 11:30:29 -0300

    v5

--- src/routes/livros.routes.js
+++ src/routes/livros.routes.js
@@ -6,12 +6,6 @@ const livrosController = new LivrosController();
 // Middlewares
 const { validarLivro, validarParamId } = require("../middleware/validar/livros.validar");
 
-//router.get("/", (req, res, next) => livrosController.listarLivros(req, res, next));
-//router.get("/:id", validarParamId, (req, res, next) => livrosController.buscarLivroPorId(req, res, next));
-//router.post("/", validarLivro, (req, res, next) => livrosController.criarLivro(req, res, next));
-//router.put("/:id", validarParamId, validarLivro, (req, res, next) => livrosController.atualizarLivro(req, res, next));
-//router.delete("/:id", validarParamId, (req, res, next) => livrosController.removerLivro(req, res, next));
-
 router.get("/", livrosController.listarLivros.bind(livrosController));
 router.get("/:id", validarParamId, livrosController.buscarLivroPorId.bind(livrosController));
 router.post("/", livrosController.criarLivro.bind(livrosController));
